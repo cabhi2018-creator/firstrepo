9b2d6cd53f4a4edc1de3ee4ab4292c2c135dc8c1
abhi <cabhi2018@gmail.com>
2026-06-14 11:01:09 +0530

    This is my second commit

--- src/main/java/test/Dashboard/First_Program.java
+++ src/main/java/test/Dashboard/First_Program.java
@@ -3,5 +3,8 @@ package test.Dashboard;
 public class First_Program {
  public static void main(String[] args) {
 	System.out.println("yes we are in success track");
+	
+	
+	System.out.println("We got succedd");
 }
 }
---
+++ src/main/java/test/Dashboard/Second_Program.java
@@ -0,0 +1,7 @@
+package test.Dashboard;
+
+public class Second_Program {
+public static void main(String[] args) {
+	System.out.println("Let's learn it");
+}
+}
