d07a6ab58a297100e4a87209446589293100b9ef
abhi <cabhi2018@gmail.com>
2026-06-14 16:56:42 +0530

    branch developement

--- src/main/java/test/Dashboard/Thirdclass.java
+++ src/main/java/test/Dashboard/Thirdclass.java
@@ -3,5 +3,19 @@ package test.Dashboard;
 public class Thirdclass {
 public static void main(String[] args) {
 	System.out.println("jelsjbkdqa");
+	
+	System.out.println("jelsjbkdqa");
+	System.out.println("jelsjbkdqa");
+	System.out.println("jelsjbkdqa");
+	System.out.println("jelsjbkdqa");
+	System.out.println("jelsjbkdqa");
+	
+	Thirdclass t = new Thirdclass();
+System.out.println(t.add(2,5));	
+}
+public int add(int b, int c)
+{
+	int a = b + c;
+	return a;
 }
 }
